c383f644b70325d4c4b497166e63c585ba92b67c
Dunk Hsieh Cheng-Lin <dunk.hsieh@titansoft.com.sg>
2017-09-13 10:34:08 +0800

    DeadAnts

---
+++ DeadAnt/DeadAnts.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DeadAnts
+{
+    class DeadAnts
+    {
+        public static int deadAntCount(String ants)
+        {
+            int IsNotAnt = 0;
+            if (ants == null)
+            {
+                return IsNotAnt;
+            }
+            string ReplaceAntToSpace = ants.Replace("ant", "");
+            IsNotAnt = "ant".Select(x => ReplaceAntToSpace.Count(c => c == x)).Max();
+            return IsNotAnt;
+        }
+    }
+}
---
+++ DeadAnt/UnitTest1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DeadAnts
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.AreEqual(0, DeadAnts.deadAntCount(null));
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            Assert.AreEqual(1, DeadAnts.deadAntCount("ant ant .... a nt"));
+        }
+
+        [TestMethod]
+        public void TestMethod3()
+        {
+            Assert.AreEqual(1, DeadAnts.deadAntCount("ant ant .... a n t"));
+        }
+        [TestMethod]
+        public void TestMethod4()
+        {
+            Assert.AreEqual(0, DeadAnts.deadAntCount("ant ant ant ant"));
+        }
+        [TestMethod]
+        public void TestMethod5()
+        {
+            Assert.AreEqual(2, DeadAnts.deadAntCount("ant anantt aantnt"));
+        }
+        [TestMethod]
+        public void TestMethod6()
+        {
+            Assert.AreEqual(2, DeadAnts.deadAntCount("ant ant .... a n t...nt"));
+        }
+    }
+}
